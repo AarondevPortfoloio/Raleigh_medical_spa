a4129d9879bee071126f24d97c6129a12f0016b1
AarondevPortfoloio <88754367+AarondevPortfoloio@users.noreply.github.com>
2023-03-24 16:45:37 -0400

    Create staff.php

---
+++ staff.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html>
+       <!-- Side navigation -->
+    <div class="sidenav">
+        <h2>Raleigh Medical Center</h2>
+        <br>
+        <a href="../index.php">Home</a>
+        <a href="pageNav.php">Log In</a>
+        <a href="apointmentRequest.php">Contact</a>
+        <a href="comments.php">Feedback</a>
+        <a href="staff.php">Staff</a>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+       
+        <p> At Raleigh medical center we take pride in making sure our patients are our top priority! </p>
+    </div>
+        <!--Page View-->
+    <div class="main">
+        <head>
+                <title>Staff</title>
+        </head>
+        <body>
+            <link rel="stylesheet" type="text/css" href="../style.css"/>
+            <center>
+            <br></br>
+                <h1>Meet The Doctors</h1>
+                <hr>
+                    <!-- First member of the team 
+                    <div class="row">
+                        <div class="column" id="gfg">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. Jan Jones</h2>
+                                    <p>Family Medicine</p>
+                                    <p>
+                                    Specialized Family and
+                                    generalized care
+                                    </p>
+                                    <button class="button">View</button>
+                                </div>
+                            </div>
+                        </div>
+                    -->
+                    <!-- Other members of the team -->
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. Nicole Hannahs</h2>
+                                    <p>Rheumatology</p>
+                                    <p>
+                                        Focus on Rheumatology and other
+                                        related health concerns.
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    
+                    <!-- Other members of the team -->
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. James Dixon</h2>
+                                    <p>General Pratice/Family Medicine</p>
+                                    <p>
+                                        General Medicine and Primary Care 
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+			        <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. James Yi</h2>
+                                    <p>General Pratice/Family Medicine</p>
+                                    <p>
+                                        Primary Care Medicine
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. Frank Smith</h2>
+                                    <p>Cardiology</p>
+                                    <p>
+                                        Specialized Cardiologist
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. Dave Walters</h2>
+                                    <p>Cardiology</p>
+                                    <p>
+                                        Cardiologist and Specialized Pacemaker
+                                        support
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                   
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. Margie Malone</h2>
+                                    <p>Gastroenterologist</p>
+                                    <p>
+                                        Gastroenterology and related health concerns 
+                                    </p>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. Shelly Samuelson</h2>
+                                    <p>General Pratice/Family Medicine</p>
+                                    <p>
+                                        Primary Care Physician
+                                    </p>
+                                    
+                               </div>
+                            </div>
+                        </div>
+                    
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. Anna Robertson</h2>
+                                    <p>General Pratice/Family Medicine</p>
+                                    <p>
+                                        Primary Care Medicine
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. Matthew Meyer</h2>
+                                    <p>Gastroenterologist</p>
+                                    <p>
+                                        Gastroenterology and other related Health Concerns
+                                    </p>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                   
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. Daniele Jameson</h2>                       
+                                    <p>General Pratice/Family Medicine</p>
+                                    <p>
+                                        Primary Care Physician
+                                    </p>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                    <h2>Dr. Dwight Sawin</h2>                        
+                                    <p>Endocrinology</p>
+                                    <p>
+                                        Endocrinology and other Related Health Concerns  
+                                    </p>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                   
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <i class="fa fa-user-circle"
+                                style="font-size:68px;"></i>
+                                <div class="container">
+                                <h2>Dr. Jack Benne</h2>
+                                <p>General Practice/Family Medicine</p>
+                                <p>
+                                    Primary Care Physician
+                                    Sports Medicine
+                                </p>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </hr>
+            </center>
+        </body>
+    </div>
+</html>
