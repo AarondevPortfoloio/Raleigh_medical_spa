276775ffc7f9cf56fb9e89ff3a7f4a309ca09217
AarondevPortfoloio <88754367+AarondevPortfoloio@users.noreply.github.com>
2023-03-24 16:44:47 -0400

    Create password.php

---
+++ password.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+       <!-- Side navigation -->
+    <div class="sidenav">
+        <h2>Raleigh Medical Center</h2>
+        <br>
+        <a href="../index.php">Home</a>
+        <a href="pageNav.php">Log In</a>
+        <a href="apointmentRequest.php">Contact</a>
+        <a href="comments.php">Feedback</a>
+        <a href="staff.php">Staff</a>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+       
+        <p> At Raleigh medical center we take pride in making sure our patients are our top priority! </p>
+    </div>
+        <!--Page View-->
+    <div class="main">
+        <center>
+        <head>
+            <title>Password Reset</title>
+        </head>
+        <body>
+        <br></br>
+            <h1>So you forgot your password?</h1>
+            <link rel="stylesheet" type="text/css" href="../style.css"/>
+            <p></p>
+            <hr>
+        </body>
+        <div class="login">
+            <form id="login" method="get" action="login.php">
+                <br>
+                <label><b>User Name: </b></label>
+                <input type="text" name="uName" id="uName" placeholder="User Name">    
+                <br><br>   
+                <input type="button" name="log" id="log" value="Get Password">  
+                <br></br>     
+            </form>
+            </hr>
+        </center>
+        </div>
+    </div>
+</html>
