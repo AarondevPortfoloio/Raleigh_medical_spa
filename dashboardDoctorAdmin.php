0d1a94be431ecd8f9ecd4649e9b332a8a2d9d47d
AarondevPortfoloio <88754367+AarondevPortfoloio@users.noreply.github.com>
2023-03-24 16:39:24 -0400

    create dashboard doctor admin

---
+++ dashboardDoctorAdmin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+       <!-- Side navigation -->
+    <div class="sidenav">
+        <h2>Raleigh Medical Center</h2>
+        <br>
+        <a href="../index.php">Home</a>
+        <a href="pageNav.php">Log In</a>
+        <a href="apointmentRequest.php">Contact</a>
+        <a href="comments.php">Feedback</a>
+        <a href="staff.php">Staff</a>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+       
+        <p> At Raleigh medical center we take pride in making sure our patients are our top priority! </p>
+    </div>
+        <!--Page View-->
+    <div class="main">
+        <center>
+        <head>
+            <title>Doctor Dashboard</title>
+        </head>
+        <body>
+        <br></br>
+            <h1>Doctor Home Page</h1>
+            <hr>
+            <link rel="stylesheet" type="text/css" href="../style.css"/>
+            <p></p>
+            </hr>
+        </body>
+        </center>
+    </div>
+</html>
