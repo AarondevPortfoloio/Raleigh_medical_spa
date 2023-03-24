95f0e3cbffb9da93977999d8fa55e4eb3b55f467
AarondevPortfoloio <88754367+AarondevPortfoloio@users.noreply.github.com>
2023-03-24 16:32:09 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <div class="h1_index">
+    <br></br>
+    <h1>Welcome to Raleigh Medical Center</h1>
+    <br></br>
+  </div>
+       <!-- Side navigation -->
+    <div class="sidenav">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+        <h2>Raleigh Medical Center</h2>
+        <br>
+        <a href="index.php">Home</a>
+        <a href="view/pageNav.php">Log In</a>
+        <a href="view/apointmentRequest.php">Contact</a>
+        <a href="view/comments.php">Feedback</a>
+        <a href="view/staff.php">Staff</a>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+       
+        <p> At Raleigh medical center we take pride in making sure our patients are our top priority! </p>
+    <!--</div>
+       <div class="fill">
+        <img  src= "model/Raleigh_Medicalhome1.png"/>
+    </div>-->
+</html>
